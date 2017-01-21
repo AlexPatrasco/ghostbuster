62e6cdb72fc3ad0336582added324c82948db084
Alex Patrasco <alexandru.agp@gmail.com>
2017-01-21 14:40:18 +0200

    WIP: create logins

--- app/controllers/user/logins_controller.rb
+++ app/controllers/user/logins_controller.rb
@@ -1,2 +1,23 @@
 class User::LoginsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    response = SpectreClient.new.request('get', Settings.API.Spectre.requests.logins.index, data:{customer_id: current_user.customer_id})
+    @logins = JSON.parse(response.body)
+  end
+
+  def add_login
+    response = SpectreClient.new.request('post', Settings.API.Spectre.requests.logins.index, data:{
+        country_code: 'XF',
+        provider_code: 'fakebank_simple_xf',
+        customer_id: current_user.customer_id
+      }
+    )
+    if response.code == 200
+      redirect_to action: :index
+    else
+      flash[:notice] = 'An error occured while creating login!'
+      redirect_to action: :index
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
   root 'navigation#home'
+  namespace :user do
+    get 'logins' => 'logins#index'
+    get 'logins/new' => 'logins#add_login', as: :new_login
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
