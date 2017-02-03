03a85d2ac5872184f8fdb863c3bac975d5dc4897
Alex Patrasco <alexandru.agp@gmail.com>
2017-02-03 14:15:51 +0200

    change return url

--- app/models/spectre_client.rb
+++ app/models/spectre_client.rb
@@ -33,7 +33,7 @@ class SpectreClient
 
   def fetch_token(entity_id, entity_type, token_type)
     token_url = Settings.API.Spectre.base_url + "tokens/#{token_type}"
-    request('post', token_url, data: {"#{entity_type}_id": entity_id, javascript_callback_type: 'iframe', return_to: user_logins_url(host: 'morning-headland-56331.herokuapp.com')})
+    request('post', token_url, data: {"#{entity_type}_id": entity_id, javascript_callback_type: 'iframe', return_to: 'http://morning-headland-56331.herokuapp.com/user/logins')})
   end
 
   def fetch_logins(customer_id)
